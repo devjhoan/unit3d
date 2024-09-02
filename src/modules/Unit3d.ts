2d6c56cae205d8cd1fec7359cca5cf5cb04b1c07
DevJhoan <znotjhoan@gmail.com>
2024-09-02 03:18:41 -0500

    chore: :fire: remove debug console code

--- src/modules/Unit3d.ts
+++ src/modules/Unit3d.ts
@@ -47,7 +47,6 @@ export class Unit3d {
 		params,
 	}: { url: string; params?: Record<string, unknown> }): Promise<T> {
 		const queryString = buildQueryString(params || {}, this.apiKey);
-		console.log(`${this.apiUrl}/api/${url}?${queryString}`);
 		const response = await fetch(`${this.apiUrl}/api/${url}?${queryString}`, {
 			headers: {
 				"Content-Type": "application/json",
